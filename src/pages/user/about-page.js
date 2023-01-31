fcf103fefb174243cb6d10be62725d8157e43fc1
Kayra Emiroglu <kemiroglu7@gmail.com>
2023-01-31 22:48:32 +0300

    About Page

--- src/pages/user/about-page.js
+++ src/pages/user/about-page.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Spacer from '../../components/common/spacer/spacer'
+import BestOffers from '../../components/user/about/best-offers/best-offers'
+import Team from '../../components/user/about/team/team'
+import WhatWeDo from '../../components/user/about/what-we-do/what-we-do'
+import PageHeader from '../../components/user/common/page-header/page-header'
 import UserTemplate from '../../templates/user-template'
 
 const AboutPage = () => {
   return (
     <UserTemplate>
-        AboutPage
-
+      <PageHeader title="About Us"/>
+      <Spacer/>
+      <BestOffers/>
+      <Spacer/>
+      <Team/>
+      <Spacer/>
+      <WhatWeDo/>
     </UserTemplate>
   )
 }
--- src/pages/user/contact-page.js
+++ src/pages/user/contact-page.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Spacer from '../../components/common/spacer/spacer'
+import PageHeader from '../../components/user/common/page-header/page-header'
 import UserTemplate from '../../templates/user-template'
 
 const ContactPage = () => {
   return (
     <UserTemplate>
-        ContactPage
+        <PageHeader title="Contact Us"/>
+        <Spacer/>
     </UserTemplate>
   )
 }
