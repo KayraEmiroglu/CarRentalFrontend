395feb78e16f6d24c9c73739f44bdb511429d4aa
Kayra Emiroglu <kemiroglu7@gmail.com>
2023-01-31 20:43:31 +0300

    Team Members

--- src/pages/user/home-page.js
+++ src/pages/user/home-page.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import Spacer from '../../components/common/spacer/spacer'
 import BestOffers from '../../components/user/about/best-offers/best-offers'
+import Team from '../../components/user/about/team/team'
 import Slider from '../../components/user/home/slider/slider'
 import UserTemplate from '../../templates/user-template'
 
 const HomePage = () => {
   return (
     <UserTemplate>
-        <Slider/>
-        <Spacer/>
-        <BestOffers/>
-        <Spacer/>
+      <Slider/>
+      <Spacer/>
+      <BestOffers/>
+      <Spacer/>
+      <Team/>
+      <Spacer/>
     </UserTemplate>
-
-
   )
 }
 
